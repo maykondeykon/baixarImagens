c771c1855c8d5b6c35208cbd2eb184219701bdb5
Maykon Deykon <maykondeykon@gmail.com>
2014-05-26 17:51:51 -0300

    adicionado interação com o usuário

--- baixar.sh
+++ baixar.sh
@@ -1,32 +1,171 @@
 #!/bin/bash
 ######################################################
 ## baixar.sh - Baixa imagens sequenciais de sites   ##
-## Escrito por: Maykon Deykon)                      ##
+## Data: 26/05/2014                                 ##
+## Escrito por: Maykon Deykon                       ##
 ## E-mail: maykondeykon@gmail.com                   ##
 ## OpenSuse GNU/Linux 13.1                          ##
 ######################################################
 
 # URL da página
-url="http://www.empireonline.com/images/image_index/original"
+url=""
+# http://www.empireonline.com/images/image_index/original/65629.jpg
 # nome da primeira imagem
-init=65628
+init=""
 # Quantidade de imagen a baixar
-qt=2
+qt=""
 # extensão do arquivo
-ext="jpg"
+ext=""
 # renomear o arquivo (sem espaços)
-name="star_wars" 
+name=""
+temp=.temp
+destino=$HOME/Downloads
+# destino="$HOME"
 
-max=$(($init + $qt -1))
-j=1
-for i in `seq $init $max`
-do
-	wget -c $url/$i.$ext
 
-	if [ ! -z $name ]; then
-		mv $i.$ext $name"-"$j.$ext
-		j=$(( $j + 1 ))
-	fi
+
+Principal(){
 	
-done
+	GetUrl	
+	# Teste
+}
 
+Teste(){
+	echo -n "Informe a pasta de destino: "
+	read dir
+	if [  -d $HOME/$dir ]
+		then
+		clear
+		SetName
+	else
+		echo $HOME/$dir' nao existe, deseja cria-la? [s,n]'
+		read cria
+		if [  $cria = 's' ]
+			then
+			echo "criando pasta..."
+			destino=$HOME/$dir
+			mkdir -p $destino
+			sleep 1
+			clear
+			SetName
+		else
+			clear
+			Teste
+			
+		fi
+	fi
+}
+
+GetUrl(){
+	echo -n "Informe a URL do site: "
+	read url
+	if [  ! -z $url ]
+		then
+
+		if curl --output /dev/null --silent --head --fail "$url"
+			then
+			GetNome
+		else
+			echo 'Informe uma URL valida!'
+			sleep 2
+			clear
+			GetUrl
+		fi
+
+	else
+		echo 'Informe uma URL valida!'
+		sleep 2
+		clear
+		GetUrl
+	fi
+}
+GetNome(){
+	echo -n "Informe o nome da primeira imagem: "
+	read init
+	if [  ! -z $init ]
+		then
+		ext="${init#*.}"
+		init="${init%%.*}"
+		GetQtd
+	else
+		echo 'Informe um nome valido!'
+		sleep 2
+		clear
+		GetNome
+	fi
+}
+GetQtd(){
+	echo -n "Quantas imagens devem ser baixadas? "
+	read qt
+	if [  ! -z $qt ]
+		then
+		setDiretorio
+	else
+		echo 'Informe a quantidade!'
+		sleep 2
+		clear
+		GetQtd
+	fi
+}
+SetName(){
+	echo -n "Informe aqui o novo nome do arquivo, ou deixe em branco: "
+	read name
+	Download
+}
+setDiretorio(){
+	echo -n "Informe a pasta de destino: "
+	read dir
+	if [  -d $HOME/$dir ]
+		then
+		clear
+		SetName
+	else
+		echo $HOME/$dir' nao existe, deseja cria-la? [s,n]'
+		read cria
+		if [  $cria = 's' ]
+			then
+			echo "criando pasta..."
+			destino=$HOME/$dir
+			mkdir -p $destino
+			sleep 1
+			clear
+			SetName
+		else
+			clear
+			Teste
+			
+		fi
+	fi
+}
+
+Download(){
+	mkdir -p $temp
+	max=$(($init + $qt -1))
+	j=1
+	for i in `seq $init $max`
+	do
+		wget -q --spider $url/$i.$ext
+
+		if [  $? = 0 ]
+			then
+			wget -c -P .temp $url/$i.$ext
+
+			if [  ! -z $name ]
+				then
+				mv $temp/$i.$ext $destino/$name"-"$j.$ext
+				j=$(( $j + 1 ))
+			else
+				mv $temp/$i.$ext $destino/
+			fi
+
+		else
+			echo 'Arquivo Nao encontrado!'
+		fi
+		
+	done
+	rm -Rf $temp
+}
+
+Principal
+
+sleep 10
