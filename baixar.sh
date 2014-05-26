195a2a6f18d354b1f443abe3e076a0c48da4b7df
Maykon Deykon <maykondeykon@gmail.com>
2014-05-26 11:53:14 -0300

    adicionar arquivo

---
+++ baixar.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+######################################################
+## baixar.sh - Baixa imagens sequenciais de sites 	##
+## Escrito por: Maykon Deykon)						##
+## E-mail: maykondeykon@gmail.com					##
+## OpenSuse GNU/Linux 13.1 							##
+######################################################
+
+# URL da página
+url="http://www.empireonline.com/images/image_index/original"
+# nome da primeira imagem
+init=65628
+# Quantidade de imagen a baixar
+qt=2
+# extensão do arquivo
+ext="jpg"
+# renomear o arquivo (sem espaços)
+name="star_wars" 
+
+max=$(($init + $qt -1))
+j=1
+for i in `seq $init $max`
+do
+	wget -c $url/$i.$ext
+
+	if [ ! -z $name ]; then
+		mv $i.$ext $name"-"$j.$ext
+		j=$(( $j + 1 ))
+	fi
+	
+done
+
+
+sleep 10
